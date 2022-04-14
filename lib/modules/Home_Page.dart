b1a734982e592ba61cba67f8792d0f7955a77a3a
flutter-developer <flutterdeveloper@gitlab.com>
2022-04-14 19:16:36 +0530

    first commit

---
+++ lib/modules/Home_Page.dart
@@ -0,0 +1,56 @@
+import 'package:cryptoapp/controller/Crypto_Controller.dart';
+import 'package:cryptoapp/modules/Crypto_Card.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  final CryptoController cryptocontroller = Get.put(CryptoController());
+
+  @override
+  void initState() {
+    cryptocontroller.loading = true;
+    cryptocontroller.fetchMovies();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text("Crypto App"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: Column(
+          children: [
+            GetBuilder(
+              builder: (CryptoController controller) {
+                return (controller.loading)
+                    ? Container()
+                    : Expanded(
+                        child: ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          itemCount: controller.moviesList.length,
+                          itemBuilder: (context, index) {
+                            return Cryptocard(
+                                cryptocontroller.moviesList[index]);
+                          },
+                        ),
+                      );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
